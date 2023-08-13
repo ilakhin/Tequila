707a317b453e7a8cc8602b7001f79c094e6ac704
Iaroslav Lakhin <iaroslav.lakhin@gmail.com>
2023-08-13 18:13:32 +0700

    Update extensions

--- Package/Scripts/Editor/Extensions/SerializedObjectExtensions.cs
+++ Package/Scripts/Editor/Extensions/SerializedObjectExtensions.cs
@@ -1,16 +1,12 @@
-﻿using JetBrains.Annotations;
-using System.Collections.Generic;
-using Tequila.Utilities;
+﻿using System.Collections.Generic;
 using UnityEditor;
 
 namespace Tequila.Extensions
 {
     public static class SerializedObjectExtensions
     {
-        public static IEnumerable<SerializedProperty> GetSerializedProperties([NotNull] this SerializedObject serializedObject, bool enterChildren, bool onlyVisible)
+        public static IEnumerable<SerializedProperty> GetSerializedProperties(this SerializedObject serializedObject, bool enterChildren, bool onlyVisible)
         {
-            ExceptionUtility.ThrowArgumentNullException_IfNull(serializedObject, nameof(serializedObject));
-
             using var iterator = serializedObject.GetIterator();
 
             if (onlyVisible)
