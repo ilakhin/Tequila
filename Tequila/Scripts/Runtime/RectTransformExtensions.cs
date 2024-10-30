6fc84fdb0efadd4a3fa006f7cf5e177c7abdbac6
Iaroslav Lakhin <iaroslav.lakhin@gmail.com>
2024-10-30 21:31:04 +0300

    Update RectTransformExtensions

--- Tequila/Scripts/Runtime/RectTransformExtensions.cs
+++ Tequila/Scripts/Runtime/RectTransformExtensions.cs
@@ -1,18 +1,33 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace IL.Tequila
 {
     public static class RectTransformExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Rect GetWorldRect(this RectTransform rectTransform)
         {
-            var rect = rectTransform.rect;
-            var matrix = rectTransform.localToWorldMatrix;
+            return rectTransform.localToWorldMatrix.MultiplyRect(rectTransform.rect);
+        }
 
-            rect.min = matrix.MultiplyPoint(rect.min);
-            rect.max = matrix.MultiplyPoint(rect.max);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Resize(this RectTransform rectTransform, Vector2 size)
+        {
+            rectTransform.sizeDelta = size - rectTransform.rect.size + rectTransform.sizeDelta;
+        }
 
-            return rect;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void StretchToParent(this RectTransform rectTransform)
+        {
+            rectTransform.localPosition = Vector3.zero;
+            rectTransform.localRotation = Quaternion.identity;
+            rectTransform.localScale = Vector3.one;
+
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+
+            rectTransform.sizeDelta = Vector2.zero;
         }
     }
 }
