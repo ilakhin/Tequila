9ce9be034a0ebfe9020b7a398a6a834c252f82e7
Iaroslav Lakhin <iaroslav.lakhin@gmail.com>
2024-12-21 15:12:07 +0300

    Update ModuleExtensions

--- Tequila/Scripts/Runtime/ModuleExtensions.cs
+++ Tequila/Scripts/Runtime/ModuleExtensions.cs
@@ -10,7 +10,7 @@ namespace IL.Tequila
         {
             if (!Attribute.IsDefined(module, typeof(TAttribute)))
             {
-                attribute = default;
+                attribute = null;
 
                 return false;
             }
@@ -25,7 +25,7 @@ namespace IL.Tequila
         {
             if (!Attribute.IsDefined(module, typeof(TAttribute), inherit))
             {
-                attribute = default;
+                attribute = null;
 
                 return false;
             }
